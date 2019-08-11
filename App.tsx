d96e122097cd038ce5a2b76f1c1df34f34128c03
Vlad <vovaturcan4@gmail.com>
2019-08-11 20:28:07 +0300

    added prettier and wrote a script to format all my files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Navigator from "./navigator";
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Navigator from './navigator'
 
 export default function App() {
   return (
     <>
       <Navigator />
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
